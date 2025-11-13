e9b17fd5ca7d4b7e59d02e8e2207b0fd12404390
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-11-13 17:11:50 +0100

    correzione cartelle

--- project2/backend/server.js
+++ project2/backend/server.js
@@ -1,82 +1,109 @@
 
-// ----------------------------------------------------------------------------
-const PORT = 4000;
-require('dotenv').config()
-process.env.REFRESH_TOKEN_KEY = "213918903"; // todo move somewhere safe
-process.env.ACCESS_TOKEN_KEY = "142530983"; // todo move somewhere safe
-process.env.MODE = "prod"; //set "test" to create example users and smart-tutorings
-process.env.TEST_smart-tutoring = "yes"
-process.env.DB_ADDRESS = `mongodb://${process.env['DB_IP']}:27017/smart-tutoring-db`;
-// ----------------------------------------------------------------------------
+import express, { json, urlencoded, static as serveStatic } from 'express';
+import { join } from 'path';
+import { connect } from 'mongoose';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import expressOpenAPI from 'express-openapi-generator';
+import { writeFileSync } from 'fs';
+import { hash } from '@node-rs/argon2';
+import Admin from './models/adminModel.js';
+import idProjection from './controller/idProjection.js';
+import { createSocketIoServer } from './controller/chat/index.js'
 
 
-/*
- * Required packages
- */
-const express = require('express');
-const app = express();
-const cookieParser = require('cookie-parser');
-const {requestMethod} = require("./src/auth/requestMethod");
-const http = require('http');
-const cors = require('cors');
-
-const {printServerStart} = require("./src/util/consoleUtil");
-
-const {Model} = require("./src/models/model");
-
-
-
-const corsOptions = {
-    origin: ["http://localhost:8080"],
-    credentials: true,
-    exposedHeaders: ['set-cookie'],
-}
-
-
-
-/*
- * Routers for this API
- */
-const indexRouter = require('./src/routes/indexRoutes');
-const authRoutes = require('./src/routes/authRoutes');
-const profileRoutes = require('./src/routes/profileRoutes');
-const smart-tutoringRoutes = require('./src/routes/smart-tutoringRoutes');
-const userSettingsRoutes = require('./src/routes/userSettingsRoutes');
-
-/*
- * Middlewares
- */
-app.use(cors(corsOptions)); // Add cors middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.json())
-app.use(requestMethod);
-
-/*
- * ROUTES
- */
-app.use(indexRouter);
-app.use("/auth", authRoutes);
-app.use("/profile", profileRoutes);
-app.use("/smart-tutoring", smart-tutoringRoutes)
-app.use("/userSetting", userSettingsRoutes)
-
-const server = http.createServer(app);
-
-/*
- * REAL-TIME ENVIRONMENT (example)
- */
-const {Realtime} = require('./src/realtime/api/Realtime');
-const smart-tutoringController = require("./src/controllers/smart-tutoringController");
-const {createTestEnvironment} = require("./src/auth/test/testUtility");
-const rt = new Realtime(server, smart-tutoringController);
-rt.listen();
-exports.realtime = rt;
-
-
-server.listen(PORT, async () => {
-    printServerStart(PORT);
-    if (process.env.MODE === "test") {
-        await createTestEnvironment();
-    }
+const documentBuilder = expressOpenAPI.DocumentBuilder.initializeDocument({
+  openapi: '3.0.1',
+  info: {
+    title: 'Smart Tutoring backend',
+    version: '1',
+  },
+  paths: {}, // You don't need to include any path objects, those will be generated later
 });
+const __dirname = import.meta.dirname;
+
+// Inizializziamo l'applicazione Express
+const app = express();
+
+// Configuriamo i middleware
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(serveStatic('uploads'));
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(serveStatic(join(__dirname, '../frontend/dist')));
+
+//Route per servire index.html quando si accede a "/"
+app.get('/', function (req, res) {
+  res.sendFile(join(__dirname, '../frontend/dist', 'index.html'));
+});
+
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/tutoring';
+// Connessione al database
+connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connection to database successful');
+    // Crea admin di default se non esiste
+    createDefaultAdmin();
+  })
+  .catch((error) => {
+    console.error('Errore connecting to the database:', error.message);
+  });
+
+
+// Funzione per creare l'admin di default
+async function createDefaultAdmin() {
+  try {
+    const admins = await Admin.find({}, idProjection(Admin), null).exec();
+    if (admins.length === 0) {
+      const admin = new Admin({
+        username: 'admin',
+        password: await hash('admin'),
+        firstName: 'admin',
+        lastName: 'admin',
+        hasFullPrivileges: true,
+      });
+      await Admin.create(admin, null);
+      console.log('Admin di default creato con successo.');
+    } else {
+      console.log('Admin di default già esistente.');
+    }
+  } catch (error) {
+    console.error('Errore nella creazione dell\'admin di default:', error);
+  }
+}  
+
+
+import admins from './routes/adminRouter.js';
+import lessons from './routes/lessonRouter.js';
+import reports from './routes/reportRouter.js';
+import reviews from './routes/reviewRouter.js';
+import users from './routes/userRouter.js';
+
+// Configuriamo i percorsi
+// ad esempio app.use('/trainers', trainersRouter);
+//indica che tutte le route definite nel modulo trainersRouter
+//saranno raggiungibili attraverso l'URL base /trainers.
+app.use('/api/admins', admins);
+app.use('/api/lessons', lessons);
+app.use('/api/reports', reports);
+app.use('/api/reviews', reviews);
+app.use('/api/users', users);
+
+// Avviamo il server su una porta specifica
+//  Il server Express viene avviato su una porta specifica,
+//  che è definita come variabile di ambiente process.env.PORT o come porta predefinita 3000.
+const port = process.env.PORT || 4000;
+if (process.env.GENERATE_OPENAPI !== undefined) {
+  documentBuilder.generatePathsObject(app);
+  writeFileSync("openapi.json", JSON.stringify(documentBuilder.build()));
+} else {
+  const server = app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+  app.locals.io = createSocketIoServer(server);
+}
