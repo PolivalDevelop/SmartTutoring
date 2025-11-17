f438e38a6fcc349052f4db2a2ee26589aedf6e2f
luca.cantagallo <luca.cantagallo99@gmail.com>
2025-11-17 13:10:14 +0100

    BookedView with fake results

--- smart-tutoring/src/components/AppHeader.vue
+++ smart-tutoring/src/components/AppHeader.vue
@@ -15,7 +15,7 @@
       <template v-if="isLoggedIn">
         <div class="logged-menu">
           <RouterLink to="/" class="menu-item">Home</RouterLink>
-          <RouterLink to="/prenotazioni" class="menu-item">Lezioni Prenotate</RouterLink>
+          <RouterLink to="/booked" class="menu-item">Lezioni Prenotate</RouterLink>
           <RouterLink to="/offerte" class="menu-item">Lezioni Offerte</RouterLink>
           <RouterLink to="/profile" class="menu-item">Profilo</RouterLink>
           <button class="menu-item logout-btn" @click="handleLogout">Logout</button>
--- smart-tutoring/src/routes/router.js
+++ smart-tutoring/src/routes/router.js
@@ -3,13 +3,15 @@ import HomeView from '@/views/HomeView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import LoginView from '@/views/LoginView.vue'
 import PersonalProfileView from '@/views/PersonalProfileView.vue'
+import BookedView from '@/views/BookedView.vue'
 
 
 const routes = [
   { path: '/', name: 'home', component: HomeView, meta: { showFilters: true } },
   { path: '/register', name: 'register', component: RegisterView },
   { path: '/login', name: 'login', component: LoginView },
-  { path: '/profile', name: 'profile', component: PersonalProfileView }
+  { path: '/profile', name: 'profile', component: PersonalProfileView },
+  { path: '/booked', name: 'booked', component: BookedView, meta: { showFilters: true } }
 ]
 
 const router = createRouter({
