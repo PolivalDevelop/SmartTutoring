2948a1cf0952e02cc9cdeff2ff92edc852425474
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-11-21 16:20:44 +0100

    .

--- project3/frontend/src/composables/useLessons.js
+++ project3/frontend/src/composables/useLessons.js
@@ -5,14 +5,15 @@ import { useUser } from './useUser.js'
 const { currentUser } = useUser()
 
 // Stato condiviso per tutte le lezioni
-export const lessons = ref([
+/*export const lessons = ref([
   { id: 1, title: 'Analisi', time: 'Mercoledì • 12:00', duration: '1h', price: 10, author: 'Marco Rossi', authorId: 1, bookedBy: '', bookedById: null },
   { id: 2, title: 'Programmazione in C', time: 'Giovedì • 15:00', duration: '1.5h', price: 15, author: 'Laura Bianchi', authorId: 2, bookedBy: '', bookedById: null },
   { id: 3, title: 'Inglese', time: 'Venerdì • 17:00', duration: '1h', price: 15, author: 'Luca Cantagallo', authorId: 3, bookedBy: 'Marco Rossi', bookedById: 1 },
   { id: 4, title: 'Inglese', time: 'Venerdì • 17:00', duration: '1h', price: 15, author: 'Luca Cantagallo', authorId: 3, bookedBy: '', bookedById: null },
   { id: 5, title: 'Analisi', time: 'Mercoledì • 12:00', duration: '1h', price: 10, author: 'Marco Rossi', authorId: 1, bookedBy: 'Luca Cantagallo', bookedById: 3 },
-])
+])*/
 
+export const lessons = ref([])
 // Funzione per aggiungere una lezione
 export function addLesson(lesson) {
   lessons.value.unshift({
